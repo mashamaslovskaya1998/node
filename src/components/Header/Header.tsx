06ae07ca119b3131838308dd25dcc9506cd20557
mashamaslovskaya1998 <mashamaslovskaya1998@mail.ru>
2022-04-04 00:23:11 +0300

    create files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="">
+        <a href="/">Tupe</a>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,23 @@
+import "./styles.scss";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <a href="" className="log"></a>
+      <ul>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+      <button></button>
+    </nav>
+  );
+};
+
+export default Nav;
