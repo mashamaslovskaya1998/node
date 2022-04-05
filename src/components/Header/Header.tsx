da325bdd48237c3a26790d542a27c517a2a5c4ff
mashamaslovskaya1998 <mashamaslovskaya1998@mail.ru>
2022-04-05 23:17:40 +0300

    init(maria): create new project

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,32 @@
-import "./styles.scss";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="">
-        <a href="/">Tupe</a>
-        <button></button>
-      </div>
-    </div>
+    <header className="header">
+      <h1 className="header__slider--title">The Power of Simplicity</h1>
+      <p className="header__slider--text">
+        Instead of spending time searching for the right app, our AI will bring
+        the right app to you.
+      </p>
+      <button className="header__slider--btn">Learn</button>
+      <ul className="slider__controls">
+        <li className="slider__dot">
+          <a href="#"></a>
+        </li>
+        <li className="slider__dot">
+          <a href="#"></a>
+        </li>
+        <li className="slider__dot slider__dot--active">
+          <a href="#"></a>
+        </li>
+        <li className="slider__dot">
+          <a href="#"></a>
+        </li>
+        <li className="slider__dot">
+          <a href="#"></a>
+        </li>
+      </ul>
+    </header>
   );
 };
 
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,23 +1,25 @@
-import "./styles.scss";
+import "./Nav.scss";
+import svg from "../../assets/logo.svg";
 
 const Nav = () => {
   return (
     <nav className="nav">
-      <a href="" className="log"></a>
-      <ul>
-        <li>
-          <a href=""></a>
+      <a href="#" className="nav__logo">
+        <img src={svg} alt="logo"></img>
+      </a>
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="#">Features</a>
         </li>
-        <li>
-          <a href=""></a>
+        <li className="nav__item">
+          <a href="">Partners</a>
         </li>
-        <li>
-          <a href=""></a>
+        <li className="nav__item">
+          <a href="">Stories</a>
         </li>
       </ul>
-      <button></button>
+      <button className="nav__button">Download for free</button>
     </nav>
   );
 };
-
 export default Nav;
